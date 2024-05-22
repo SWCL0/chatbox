21f43172120720fe8f84c5e128621023ab8497f1
SWCL0 <stan.w.c@icloud.com>
2024-05-22 09:28:10 +0200

    toevoeging chatbox
    comment lines toegevoegd aan de code
    
    uitlegging functies en wat bepaalde delen van code doet of waarom het er staat.
    
    html verduidelijkt door 3 secties te creeëren doormiddel van commentaar lines

--- index.js
+++ index.js
@@ -8,31 +8,46 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
+// was nodig zodat ik een apart bestand kon hebben voor styling
+// serve static files van de directory chatbox
 app.use(serveStatic(join(__dirname, 'chatbox'), {
+    // set custom http headers
+    // res = response object - path = path to the file being served
     setHeaders: (res, path) => {
+        // looks up de MIME type of the file based on its exctension
         if (serveStatic.mime.lookup(path) === 'text/css') {
+            // als het een css file is zet Content-Type header to text/css
             res.setHeader('Content-Type', 'text/css');
         }
     }
 }));
 
+// route handeler for get request
+// req = request object - res = response object
 app.get('/', (req, res) => {
+    // stuurt file als response en joint de hele path als een string
     res.sendFile(join(__dirname, 'chatbox', 'ChatRoom.html'));
 });
 
+// event listener voor connectie event op object io (io = server - socket = client connectie)
 io.on('connection', (socket) => {
-    console.log('connected');
+    // event listener voor chat message event op object socket
     socket.on('chat message', (msg) => {
+        // broadcast ontvangen bericht naar alle connected socket.io servers
         io.emit('chat message', msg);       
     });
 });
+// event listener voor connectie event op object io
 io.on('connection', (socket) => {
+    // event listerner voor naam invoer event op object socket, een argument usr
     socket.on('name', (usr) => {
+        // broadcast ontvangen naam naar alle connected socket.io servers
         io.emit('name', usr);
-        console.log(usr);
     });
 })
 
+// start server en luisters for speciefieke connecties op port 3000
 server.listen(3000, () => {
+    // als de server goed is gestart zal bericht hieronder gelogged worden
     console.log('server is running at http://localhost:3000');
 });
